2ed2e21ad8bf6189cd34d0d9f6ff3addcc16f71e
kidow <wcgo2ling@gmail.com>
2021-08-21 15:26:36 +0900

    next config env

--- next.config.js
+++ next.config.js
@@ -2,5 +2,9 @@ module.exports = {
   devIndicators: {
     autoPrerender: false
   },
-  webpack5: false
+  webpack5: false,
+  env: {
+    SUPABASE_URL: process.env.SUPABASE_URL,
+    SUPABASE_KEY: process.env.SUPABASE_KEY
+  }
 }
